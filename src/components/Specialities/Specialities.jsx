7e7cb5118e3cb2fd2d5fd8901bcf2c61ba9f6035
maksgg <maks.zhovmir.97@gmail.com>
2024-07-17 17:25:07 +0300

    feat: add new responsive style (delivery section) and re-write restaurant section again

--- src/components/Specialities/Specialities.jsx
+++ src/components/Specialities/Specialities.jsx
@@ -20,6 +20,9 @@ const Specialities = () => {
           </div>
         </div>
         <div className='rest_container22'>
+          <div className='arrow3'>
+            <img src={arrLeft} alt="Arrow" />
+          </div>
           <div className='card-preview1'>
             <img 
             className='rest-img1'
@@ -27,68 +30,61 @@ const Specialities = () => {
             alt="Food" />
             <p className='specialities-text'>Bagel</p>
           </div>
-          <div className='card-preview1'>
+          <div className='card-preview1 s25'>
             <img 
             className='rest-img1'
             src={s2} 
             alt="Food" />
             <p className='specialities-text'>Burger</p>
           </div>
-          <div className='card-preview1'>
+          <div className='card-preview1 s24'>
             <img 
             className='rest-img1'
             src={s3} 
             alt="Food" />
             <p className='specialities-text'>Chicken</p>
           </div>
-          <div className='card-preview1'>
+          <div className='card-preview1 s24'>
             <img 
             className='rest-img1'
             src={s4} 
             alt="Food" />
             <p className='specialities-text'>Fish</p>
           </div>
-          <div className='card-preview1'>
+          <div className='card-preview1 s23'>
             <img 
             className='rest-img1'
             src={s5} 
             alt="Food" />
             <p className='specialities-text'>Fish`Chips</p>
           </div>
-          <div className='card-preview1'>
+          <div className='card-preview1 s23'>
             <img 
             className='rest-img1'
             src={s6} 
             alt="Food" />
             <p className='specialities-text'>Salads</p>
           </div>
-          <div className='card-preview1'>
+          <div className='card-preview1 s22'>
             <img 
             className='rest-img1'
             src={s7} 
             alt="Food" />
             <p className='specialities-text'>Pizza</p>
           </div>
-          <div className='card-preview1'>
+          <div className='card-preview1 s22'>
             <img 
             className='rest-img1'
             src={s8} 
             alt="Food" />
             <p className='specialities-text'>Pasta</p>
           </div>
-          
-        
-        </div>
-        <div className='arrow-container1'>
-            <div className='arrow3'>
-              <img src={arrLeft} alt="Arrow" />
-            </div>
-            <div className='arrow3'>
-              <img src={arrRight} alt="Arrow" />
-            </div>
+          <div className='arrow3'>
+            <img src={arrRight} alt="Arrow" />
           </div>
+        </div>
     </section>
   )
 }
 
-export default Specialities
+export default Specialities;
